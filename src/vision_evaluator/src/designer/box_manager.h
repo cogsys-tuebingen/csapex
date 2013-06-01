a18ee0fd5590b0b5ce88ee35fce12243892a2751
buck <buck@690284e3-2886-4bbc-99f2-a987503dca67>
2013-06-01 15:39:45 +0000

    began extraction of utility classes
    git-svn-id: https://rarepos.cs.uni-tuebingen.de/svn-path/rabot/trunk/catkin@4794 690284e3-2886-4bbc-99f2-a987503dca67

--- src/vision_evaluator/src/designer/box_manager.h
+++ src/vision_evaluator/src/designer/box_manager.h
@@ -4,7 +4,15 @@
 class BoxManager
 {
 public:
+    static BoxManager& instance() {
+        static BoxManager inst;
+        return inst;
+    }
+
+protected:
     BoxManager();
+    BoxManager(const BoxManager& copy);
+    BoxManager& operator = (const BoxManager& assign);
 };
 
 #endif // BOX_MANAGER_H
