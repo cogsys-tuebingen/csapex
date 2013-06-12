7f811fe705902c3c0cce8d4eb70d448744301c82
buck <buck@690284e3-2886-4bbc-99f2-a987503dca67>
2013-06-12 12:07:47 +0000

    ported cv demo of lk tracking to a filter
    git-svn-id: https://rarepos.cs.uni-tuebingen.de/svn-path/rabot/trunk/catkin@4817 690284e3-2886-4bbc-99f2-a987503dca67

---
+++ src/cip104/src/filter_lk_tracking.h
@@ -0,0 +1,45 @@
+#ifndef FILTER_TOOL_DETECTION_H
+#define FILTER_TOOL_DETECTION_H
+
+/// COMPONENT
+#include <vision_evaluator/filter.h>
+
+/// SYSTEM
+#include <QSlider>
+
+namespace cip104
+{
+
+class LKTracking : public vision_evaluator::Filter
+{
+    Q_OBJECT
+
+public:
+    LKTracking();
+
+public:
+    virtual void filter(cv::Mat& img, cv::Mat& mask);
+    virtual void insert(QBoxLayout* layout);
+
+public Q_SLOTS:
+    void update();
+
+protected:
+    void init(cv::Mat &gray, cv::Mat &mask);
+
+private:
+    bool init_;
+
+    std::vector<cv::Point2f> points[2];
+    int max_count;
+    cv::TermCriteria termcrit;
+    cv::Size subPixWinSize;
+    cv::Size winSize;
+
+    cv::Mat gray;
+    cv::Mat prevGray;
+};
+
+}
+
+#endif // FILTER_TOOL_DETECTION_H
