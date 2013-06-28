ce6d79b4f85a9f31a607cb811b8a054f49d0bde7
hanten <hanten@690284e3-2886-4bbc-99f2-a987503dca67>
2013-06-28 15:29:14 +0000

    forgot some
    git-svn-id: https://rarepos.cs.uni-tuebingen.de/svn-path/rabot/trunk/catkin@4861 690284e3-2886-4bbc-99f2-a987503dca67

--- src/utils/src/LibCvTools/histogram.hpp
+++ src/utils/src/LibCvTools/histogram.hpp
@@ -43,102 +43,17 @@ inline void normalize_rgb(const cv::Mat &src, cv::Mat &dst)
 
 }
 
-
-/**
- * @brief Generates histogram images with certain parameters.
- * @param image         the image to generate a histogram of no special color room required
- * @param histograms    the histograms will be written to here
- * @param bins          the amount of bins per channel - if only one given, it will be used for all
- * @param ranges        range from 0 to given number for each channel - if only one given, it will be used for all
- * @param normalize     normalization values for each channels - if only one given, it  will be used for all
- * @param uniform       if the histogram should be uniform
- * @param accumulate    if the histogram should be calculated accumulative
- */
-inline void generate_histogram(const cv::Mat &image, std::vector<cv::Mat> &histograms, const cv::Mat mask,
-                               const std::vector<int> &bins, const std::vector<int> &ranges,
-                               const std::vector<float> &normalize,
-                               const bool uniform = false, bool accumulate = true)
+inline void generate_histogram(const std::vector<cv::Mat> &images, std::vector<cv::Mat> &histograms, const cv::Mat mask,
+                               const std::vector<int> &bins, const std::vector<float> &ranges,
+                               bool accumulate = true)
 {
-    int norm_factor_count   = normalize.size();
-    int plane_count         = image.channels();
-    int bins_count          = bins.size();
-    int range_count         = ranges.size();
-
-    std::vector<cv::Mat> planes;
-    cv::split(image, planes);
-    histograms.resize(plane_count);
-
-    std::vector< std::vector<float> > hist_ranges;
-    hist_ranges.resize(range_count);
-    for(int i = 0 ; i < range_count; i++) {
-        hist_ranges[i].push_back(0.f);
-        hist_ranges[i].push_back(ranges[i]);
-    }
-    for(unsigned int i = 0 ; i < plane_count ; i++) {
-        int b = bins_count > 1 ? bins[i] : bins[0];
-        const float* r = { range_count > 1 ? (float*) hist_ranges[i].data() : (float*) hist_ranges[0].data() };
-        cv::calcHist(&planes[i], 1, 0, mask, histograms[i], 1, &b, &r, uniform, accumulate);
+    std::vector<int> channels;
+    for(std::vector<cv::Mat>::const_iterator it = images.begin(); it != images.end() ; it++) {
+        channels.push_back(it->channels());
     }
 
-    if(norm_factor_count > 0) {
-        for(unsigned int i = 0 ; i < histograms.size(); i++) {
-            float n = norm_factor_count > 1 ? normalize[i] : normalize[0];
-            cv::normalize(histograms[i], histograms[i], 0, n, cv::NORM_MINMAX, -1, cv::Mat() );
-        }
-    }
-}
+    cv::calcHist(images, channels, mask, histograms, bins, ranges, accumulate);
 
-inline void generate_histogram(const cv::Mat &image, std::vector<cv::Mat> &histograms,
-                               const std::vector<int> &bins, const std::vector<int> &ranges,
-                               const std::vector<float> &normalize,
-                               const bool uniform = false, bool accumulate = true)
-{
-    generate_histogram(image, histograms, cv::Mat(), bins,  ranges, normalize, uniform, accumulate);
-}
-
-/**
- * @brief Genearate a histogram for a rgb image return bins in vectors.
- * @param image             the image to generate a histogram of
- * @param histograms        to save the bins to
- * @param bins              the amount of bins for each channel
- * @param normalize         the normalization factor for all channels
- * @param uniform           if the histogram should be uniform
- * @param accumulate        if the histogram should be calculated accumulative
- */
-inline void generate_histogram_rgb(const cv::Mat &image, std::vector<cv::Mat> &histograms, const int bins = 256,
-                                   const float normalize = 0.f, const bool uniform = true, bool accumulate = false)
-{
-    std::vector<int>    bins_per_channel;
-    std::vector<float>  norm_per_channel;
-    std::vector<int>    hist_range_per_channel;
-    bins_per_channel.push_back(bins);
-    if(normalize > 0.f)
-        norm_per_channel.push_back(normalize);
-    hist_range_per_channel.push_back(256);
-
-    generate_histogram(image, histograms, bins_per_channel,hist_range_per_channel, norm_per_channel, uniform, accumulate);
-}
-
-/**
- * @brief Genearate a histogram for a rgb image return bins in vectors.
- * @param image             the image to generate a histogram of
- * @param histogram_bins    to save the bins to
- * @param bins              the amount of bins for each channel
- * @param normalize         the normalization factor for all channels
- * @param uniform           if the histogram should be uniform
- * @param accumulate        if the histogram should be calculated accumulative
- */
-inline void generate_histogram_rgb(const cv::Mat &image, std::vector<std::vector<float> > &histogram_bins, const int bins = 256,
-                                   const float normalize = 0.f, const bool uniform = true, bool accumulate = false)
-{
-    std::vector<cv::Mat> histograms;
-    generate_histogram_rgb(image, histograms, bins, normalize, uniform, accumulate);
-    for(int i = 0 ; i < histograms.size() ; i++) {
-        histogram_bins.push_back(std::vector<float>());
-        for(int j = 0 ; j < bins ; j++) {
-            histogram_bins[i].push_back(histograms[i].at<float>(j));
-        }
-    }
 }
 
 /**
