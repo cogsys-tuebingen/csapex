33b8c501a369821b044ffa93c3537a2e38933f8f
hanten <hanten@690284e3-2886-4bbc-99f2-a987503dca67>
2013-07-04 17:35:00 +0000

    fixes
    git-svn-id: https://rarepos.cs.uni-tuebingen.de/svn-path/rabot/trunk/catkin@4879 690284e3-2886-4bbc-99f2-a987503dca67

--- src/vision_plugins/src/filter_perspective.cpp
+++ src/vision_plugins/src/filter_perspective.cpp
@@ -23,25 +23,52 @@ void PerspectiveTransform::filter(cv::Mat &img, cv::Mat &mask)
 
 void PerspectiveTransform::insert(QBoxLayout *parent)
 {
-    rot_x_ = QtHelper::makeDoubleSlider(parent, "Rotation x", 0, -90, 90, 0.1);
-    rot_y_ = QtHelper::makeDoubleSlider(parent, "Rotation y", 0, -90, 90, 0.1);
-    rot_z_ = QtHelper::makeDoubleSlider(parent, "Rotation z", 0, -90, 90, 0.1);
-    distance_     = QtHelper::makeDoubleSlider(parent, "Virt. Distance", 500, 0, 2000, 0.1);
-    focal_length_ = QtHelper::makeDoubleSlider(parent, "Virt. Focal Length", 500, 0, 2000, 0.1);
+    slider_rot_x_ = QtHelper::makeDoubleSlider(parent, "Rotation x", 0, -90, 90, 0.1);
+    slider_rot_y_ = QtHelper::makeDoubleSlider(parent, "Rotation y", 0, -90, 90, 0.1);
+    slider_rot_z_ = QtHelper::makeDoubleSlider(parent, "Rotation z", 0, -90, 90, 0.1);
+    slider_distance_     = QtHelper::makeDoubleSlider(parent, "Virt. Distance", 500, 0, 2000, 0.1);
+    slider_focal_length_ = QtHelper::makeDoubleSlider(parent, "Virt. Focal Length", 500, 0, 2000, 0.1);
 
-    connect(rot_x_, SIGNAL(valueChanged(int)), this, SLOT(update()));
-    connect(rot_y_, SIGNAL(valueChanged(int)), this, SLOT(update()));
-    connect(rot_z_, SIGNAL(valueChanged(int)), this, SLOT(update()));
-    connect(distance_, SIGNAL(valueChanged(int)), this, SLOT(update()));
-    connect(focal_length_, SIGNAL(valueChanged(int)), this, SLOT(update()));
+    connect(slider_rot_x_, SIGNAL(valueChanged(int)), this, SLOT(update()));
+    connect(slider_rot_y_, SIGNAL(valueChanged(int)), this, SLOT(update()));
+    connect(slider_rot_z_, SIGNAL(valueChanged(int)), this, SLOT(update()));
+    connect(slider_distance_, SIGNAL(valueChanged(int)), this, SLOT(update()));
+    connect(slider_focal_length_, SIGNAL(valueChanged(int)), this, SLOT(update()));
 
 }
 
 void PerspectiveTransform::update()
 {
-    transformer_.set_rot_x(rot_x_->doubleValue());
-    transformer_.set_rot_y(rot_y_->doubleValue());
-    transformer_.set_rot_z(rot_z_->doubleValue());
-    transformer_.set_distance(distance_->doubleValue());
-    transformer_.set_focal(focal_length_->doubleValue());
+    state_.rot_x = slider_rot_x_->doubleValue();
+    state_.rot_y = slider_rot_y_->doubleValue();
+    state_.rot_z = slider_rot_z_->doubleValue();
+    state_.foca  = slider_focal_length_->doubleValue();
+    state_.dist  = slider_distance_->doubleValue();
+    transformer_.set_rot_x(state_.rot_x);
+    transformer_.set_rot_y(state_.rot_y);
+    transformer_.set_rot_z(state_.rot_z);
+    transformer_.set_distance(state_.dist);
+    transformer_.set_focal(state_.foca);
+}
+
+Memento::Ptr PerspectiveTransform::getState() const
+{
+    boost::shared_ptr<State> memento(new State);
+    *memento = state_;
+
+    return memento;
+}
+
+void PerspectiveTransform::setState(Memento::Ptr memento)
+{
+    boost::shared_ptr<State> m = boost::dynamic_pointer_cast<State> (memento);
+    assert(m.get());
+
+    state_ = *m;
+
+    slider_rot_x_->setDoubleValue(state_.rot_x);
+    slider_rot_y_->setDoubleValue(state_.rot_y);
+    slider_rot_z_->setDoubleValue(state_.rot_z);
+    slider_distance_->setDoubleValue(state_.dist);
+    slider_focal_length_->setDoubleValue(state_.foca);
 }
--- src/vision_plugins/src/filter_perspective.h
+++ src/vision_plugins/src/filter_perspective.h
@@ -20,6 +20,9 @@ public:
     virtual void filter(cv::Mat &img, cv::Mat &mask);
     virtual void insert(QBoxLayout *parent);
 
+    void setState(Memento::Ptr memento);
+    Memento::Ptr getState() const;
+
 public Q_SLOTS:
     void update();
 
@@ -27,11 +30,41 @@ public Q_SLOTS:
 private:
     PerspectiveTransformer transformer_;
 
-    QDoubleSlider *rot_x_;
-    QDoubleSlider *rot_y_;
-    QDoubleSlider *rot_z_;
-    QDoubleSlider *distance_;
-    QDoubleSlider *focal_length_;
+    QDoubleSlider *slider_rot_x_;
+    QDoubleSlider *slider_rot_y_;
+    QDoubleSlider *slider_rot_z_;
+    QDoubleSlider *slider_distance_;
+    QDoubleSlider *slider_focal_length_;
+
+    class State : public Memento {
+    public:
+        void readYaml(const YAML::Node &node)
+        {
+            node["rot_x"] >> rot_x;
+            node["rot_y"] >> rot_y;
+            node["rot_z"] >> rot_z;
+            node["foca"] >> foca;
+            node["dist"] >> dist;
+        }
+
+        void writeYaml(YAML::Emitter &out) const
+        {
+            out << YAML::Key << "rot_x" << YAML::Value << rot_x;
+            out << YAML::Key << "rot_y" << YAML::Value << rot_y;
+            out << YAML::Key << "rot_z" << YAML::Value << rot_z;
+            out << YAML::Key << "foca" << YAML::Value << foca;
+            out << YAML::Key << "dist" << YAML::Value << dist;
+        }
+
+    public:
+        double rot_x;
+        double rot_y;
+        double rot_z;
+        double foca;
+        double dist;
+    };
+
+    State state_;
 
 };
 
