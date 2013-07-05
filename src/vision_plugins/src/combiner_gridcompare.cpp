e49bf4b8e93164ae7ad0749be66c34a0f9350de9
hanten <hanten@690284e3-2886-4bbc-99f2-a987503dca67>
2013-07-05 14:34:16 +0000

    begun histogram compare
    git-svn-id: https://rarepos.cs.uni-tuebingen.de/svn-path/rabot/trunk/catkin@4883 690284e3-2886-4bbc-99f2-a987503dca67

--- src/vision_plugins/src/combiner_gridcompare.cpp
+++ src/vision_plugins/src/combiner_gridcompare.cpp
@@ -4,7 +4,6 @@
 #include <pluginlib/class_list_macros.h>
 
 /// PROJECT
-#include <utils/LibCvTools/grid.hpp>
 #include <utils/LibCvTools/grid_attributes.hpp>
 #include <utils/LibCvTools/grid_compute.hpp>
 
@@ -12,6 +11,7 @@ PLUGINLIB_EXPORT_CLASS(vision_evaluator::GridCompare, vision_evaluator::ImageCom
 
 
 using namespace vision_evaluator;
+using namespace cv_grid;
 
 GridCompare::GridCompare()
 {
@@ -19,13 +19,37 @@ GridCompare::GridCompare()
 
 cv::Mat GridCompare::combine(const cv::Mat img1, const cv::Mat mask1, const cv::Mat img2, const cv::Mat mask2)
 {
+    if(!img1.empty() && !img2.empty()) {
+        GridHist g1, g2;
+        AttrHistogram::Params p;
+        p.bins = (cv::Mat_<int>(3,1) << 32, 32, 32);
+        p.eps  = cv::Scalar(97,97,97);
+        p.method = CV_COMP_CHISQR;
+        p.ranges = (cv::Mat_<float>(6,1) << 0.f, 256.f,0.f, 256.f,0.f, 256.f);
+        cv::Mat img1_;
+        cv::Mat img2_;
+
+        cv::cvtColor(img1, img1_, CV_BGR2YUV);
+        cv::cvtColor(img2, img2_, CV_BGR2YUV);
+
+        cv_grid::prepare_grid<AttrHistogram>(g1, img1_, 56, 72, p);
+        cv_grid::prepare_grid<AttrHistogram>(g2, img2_, 56, 72, p);
+
+
+        std::cout << img1.rows << " " << img1.cols << std::endl;
+
+        cv::Mat out(img1.rows, img1.cols, CV_8UC3, cv::Scalar(0,0,0));
+        std::pair<int, int> counts;
+        render_grid(g1, g2, out, counts);
+
+        std::cout << "COUNTS +" << counts.first << " -" << counts.second << std::endl;
+
+        return out;
+    }
     return cv::Mat();
 }
 
 void GridCompare::fill(QBoxLayout *layout)
 {
-    cv_grid::Grid g = cv_grid::GridHLS(2,2);
-    std::cout << g.rows() << " " << g.cols() << std::endl;
-
     ImageCombiner::fill(layout);
 }
--- src/vision_plugins/src/combiner_gridcompare.h
+++ src/vision_plugins/src/combiner_gridcompare.h
@@ -2,6 +2,7 @@
 #define COMBINER_GRIDCOMPARE_H
 
 #include <vision_evaluator/image_combiner.h>
+#include <utils/LibCvTools/grid.hpp>
 
 namespace vision_evaluator {
 class GridCompare : public ImageCombiner
@@ -12,6 +13,27 @@ public:
 
 protected:
     virtual void fill(QBoxLayout *layout);
+
+    template<class GridT>
+    void render_grid(const GridT &g1, const GridT &g2, cv::Mat &out, std::pair<int, int> &counts)
+    {
+        counts.first = 0;
+        counts.second = 0;
+        for(int i = 0 ; i < g1.cols() ; i++) {
+            for(int j = 0 ; j < g1.rows() ; j++) {
+                bool cell_compare = g1(j,i) == g2(j,i);
+
+                cv::Rect r = g1(j,i).bounding;
+                if(cell_compare) {
+                    cv::rectangle(out, r, cv::Scalar(0, 255, 0), 1);
+                    counts.first++;
+                } else {
+                    cv::rectangle(out, r, cv::Scalar(0, 0, 255), 1);
+                    counts.second++;
+                }
+            }
+        }
+    }
 };
 }
 #endif // COMBINER_GRIDCOMPARE_H
