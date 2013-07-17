2f164f193ecff4faf2dfd121e9f421accc83ebff
hanten <hanten@690284e3-2886-4bbc-99f2-a987503dca67>
2013-07-17 08:51:37 +0000

    forgotten function
    git-svn-id: https://rarepos.cs.uni-tuebingen.de/svn-path/rabot/trunk/catkin@4906 690284e3-2886-4bbc-99f2-a987503dca67

--- src/utils/src/LibCvTools/grid_compute.hpp
+++ src/utils/src/LibCvTools/grid_compute.hpp
@@ -57,6 +57,50 @@ typedef Grid_<GridCellScalar>                   GridScalar;        /// a grid ba
 typedef GridCell<cv::Rect, AttrHistogram>       GridCellHist;
 typedef Grid_<GridCellHist>                     GridHist;
 
+template<class GridT>
+inline void render_grid_count(const GridT &g1, const GridT &g2, cv::Mat &out, std::pair<int, int> &counts, int &valid)
+{
+    counts.first = 0;
+    counts.second = 0;
+    valid = 0;
+    for(int i = 0 ; i < g1.cols() ; i++) {
+        for(int j = 0 ; j < g1.rows() ; j++) {
+
+            cv::Rect r = g1(j,i).bounding;
+            if(!g1(j,i).enabled || !g2(j,i).enabled) {
+                cv::rectangle(out, r, cv::Scalar(0, 255, 255), 1);
+                continue;
+            }
+
+            bool cell_compare = g1(j,i) == g2(j,i);
+            if(cell_compare) {
+                cv::rectangle(out, r, cv::Scalar(0, 255, 0), 1);
+                counts.first++;
+            } else {
+                cv::rectangle(out, r, cv::Scalar(0, 0, 255), 1);
+                counts.second++;
+            }
+            valid++;
+        }
+    }
+
+    if(out.rows < 42 && out.cols < 120)
+        throw std::runtime_error("Image to small to render text!");
+    std::stringstream text;
+    text << "+" << counts.first << " | -" << counts.second << " | all: " << valid;
+    cv::putText(out, text.str(), cv::Point(0, out.rows - 2), cv::FONT_HERSHEY_SIMPLEX, 1.5, cv::Scalar(0, 255, 255), 2);
+
+}
+
+template<class GridT>
+inline void render_grid(const GridT &g1, const GridT &g2, cv::Mat &out)
+{
+     std::pair<int,int> c(0,0);
+     int v = 0;
+
+     render_grid_count<GridT>(g1, g2, out, c, v);
+}
+
 }
 
 
