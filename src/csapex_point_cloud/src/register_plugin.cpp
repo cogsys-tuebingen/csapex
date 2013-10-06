42ada380ed2849c9c6c1939b6e542086b1e6cedf
buck <sebastian.buck@uni-tuebingen.de>
2013-10-06 21:57:15 +0200

    generic pointcloud import

--- src/csapex_point_cloud/src/register_plugin.cpp
+++ src/csapex_point_cloud/src/register_plugin.cpp
@@ -14,6 +14,9 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/common/impl/io.hpp>
+#include <boost/mpl/for_each.hpp>
 
 PLUGINLIB_EXPORT_CLASS(csapex::RegisterPointCloudPlugin, csapex::CorePlugin)
 
@@ -36,10 +39,64 @@ struct Sensor2Cloud
         sensor_msgs::PointCloud2::Ptr &out_;
     };
 
+    struct try_convert
+    {
+        try_convert(const sensor_msgs::PointCloud2::ConstPtr &ros_msg, typename connection_types::PointCloudMessage::Ptr& out, bool& success)
+            : ros_msg_(ros_msg), out_(out), success_(success)
+        {
+            success_ = false;
+        }
+
+        template<typename PointT>
+        void operator()(PointT& pt)
+        {
+            if(success_) {
+                return;
+            }
+
+            std::vector<pcl::PCLPointField> fields;
+            pcl::for_each_type<typename pcl::traits::fieldList<PointT>::type>(pcl::detail::FieldAdder<PointT>(fields));
+
+            const sensor_msgs::PointCloud2::_fields_type& available_fields = ros_msg_->fields;
+
+
+            if(fields.size() != available_fields.size()) {
+                return;
+            }
+
+            for(size_t d = 0; d < fields.size (); ++d) {
+                bool found = false;
+                for(size_t f = 0; f < available_fields.size() && !found; ++f) {
+                    if(fields[d].name == available_fields[f].name) {
+                        found = true;
+                    }
+                }
+
+                if(!found) {
+                    return;
+                }
+            }
+
+            success_ = true;
+            typename pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
+            pcl::fromROSMsg(*ros_msg_, *cloud);
+            out_->value = cloud;
+        }
+
+        const sensor_msgs::PointCloud2::ConstPtr &ros_msg_;
+        typename connection_types::PointCloudMessage::Ptr& out_;
+
+        bool& success_;
+    };
+
     static void ros2apex(const sensor_msgs::PointCloud2::ConstPtr &ros_msg, typename connection_types::PointCloudMessage::Ptr& out) {
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-        pcl::fromROSMsg(*ros_msg, *cloud);
-        out->value = cloud;
+        bool success;
+        try_convert converter(ros_msg, out, success);
+        boost::mpl::for_each<connection_types::PointCloudPointTypes>( converter );
+
+        if(!converter.success_) {
+            std::cerr << "cannot convert message, type is not known" << std::endl;
+        }
     }
     static void apex2ros(const typename connection_types::PointCloudMessage::Ptr& apex_msg, sensor_msgs::PointCloud2::Ptr &out) {
         boost::apply_visitor (Export(out), apex_msg->value);
