bbf170bb25adb360850bf5dd4d030818fc4c07f4
buck <sebastian.buck@uni-tuebingen.de>
2013-10-08 14:31:25 +0200

    added time message

---
+++ src/csapex_transform/include/csapex_transform/time_stamp_message.h
@@ -0,0 +1,25 @@
+#ifndef TIME_STAMP_MESSAGE_H
+#define TIME_STAMP_MESSAGE_H
+
+/// PROJECT
+#include <csapex/model/message.h>
+
+/// SYSTEM
+#include <ros/time.h>
+
+namespace csapex {
+namespace connection_types {
+
+
+struct TimeStampMessage : public MessageTemplate<ros::Time, TimeStampMessage>
+{
+    TimeStampMessage();
+
+    void writeYaml(YAML::Emitter& yaml);
+    void readYaml(YAML::Node& node);
+};
+
+}
+}
+
+#endif // TIME_STAMP_MESSAGE_H
