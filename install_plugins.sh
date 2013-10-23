70ae46394bd440b71fe2df55316b988df10d974c
buck <sebastian.buck@uni-tuebingen.de>
2013-10-23 11:50:25 +0200

    submodules for plugins added

---
+++ install_plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ -f .gitmodules ]]; then
+	git submodule init
+	git submodule update
+else
+	git submodule add gitlab@gitlab.cs.uni-tuebingen.de:csapex/vision-plugins.git plugins/vision_plugins
+	git submodule add gitlab@gitlab.cs.uni-tuebingen.de:csapex/backgroundsubtraction.git plugins/background_subtraction
+	git submodule add gitlab@gitlab.cs.uni-tuebingen.de:csapex/robot-detection.git plugins/robot_detection
+	git submodule add gitlab@gitlab.cs.uni-tuebingen.de:csapex/cip104.git plugins/cip104
+fi
