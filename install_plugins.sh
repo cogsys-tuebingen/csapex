f1e401d13dca4e7ce67093e145ad08f3791b77e7
Sebastian Buck <sebastian.buck@uni-tuebingen.de>
2013-12-09 12:51:31 +0100

    switched from submodules to standard clone

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-if [[ -f .gitmodules ]]; then
-	echo "updating submodules"
-else
-	echo "installing submodules"
-fi
 
-if [[ -f .gitmodules ]]; then
-	git submodule init
-	git submodule update
-else
-	git submodule add gitlab@gitlab.cs.uni-tuebingen.de:csapex/vision-plugins.git plugins/vision_plugins
-	git submodule add gitlab@gitlab.cs.uni-tuebingen.de:csapex/backgroundsubtraction.git plugins/background_subtraction
-	git submodule add gitlab@gitlab.cs.uni-tuebingen.de:csapex/robot-detection.git plugins/robot_detection
-	git submodule add gitlab@gitlab.cs.uni-tuebingen.de:csapex/cip104.git plugins/cip104
-	git submodule add gitlab@gitlab.cs.uni-tuebingen.de:csapex/patsy.git plugins/patsy
-fi
+git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/vision-plugins.git plugins/vision_plugins
+git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/backgroundsubtraction.git plugins/background_subtraction
+git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/robot-detection.git plugins/robot_detection
+git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/cip104.git plugins/cip104
+git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/patsy.git plugins/patsy
