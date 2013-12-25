c16838070691df7c9fbacce211a6a24c415f4915
buck <sebastian.buck@uni-tuebingen.de>
2013-12-25 23:43:23 +0100

    refactored qt helper

--- src/csapex/include/csapex/utility/qt_helper.hpp
+++ src/csapex/include/csapex/utility/qt_helper.hpp
@@ -61,117 +61,19 @@ struct QSleepThread : public QThread {
 class QtHelper
 {
 public:
-    static QSpinBox* makeSpinBox(QBoxLayout *layout, const std::string &name, int def, int min, int max) {
-        QHBoxLayout *internal_layout = new QHBoxLayout;
+    static QSpinBox* makeSpinBox(QBoxLayout *layout, const std::string &name, int def, int min, int max) ;
 
-        QSpinBox* spinner = new QSpinBox;
-        spinner->setMinimum(min);
-        spinner->setMaximum(max);
-        spinner->setValue(def);
+    static QSlider* makeSlider(QBoxLayout* layout, const std::string& name, int def, int min, int max);
 
-        internal_layout->addWidget(new QLabel(name.c_str()));
-        internal_layout->addWidget(spinner);
-        layout->addLayout(internal_layout);
+    static QDoubleSlider* makeDoubleSlider(QBoxLayout* layout, const std::string& name, double def, double min, double max, double step_size);
 
-        return spinner;
-    }
+    static QWidget* wrapLayout(QBoxLayout *l, QWidget *parent = 0);
 
-    static QSlider* makeSlider(QBoxLayout* layout, const std::string& name, int def, int min, int max) {
-        QHBoxLayout* internal_layout = new QHBoxLayout;
+    static QHBoxLayout* wrap(const std::string& label, QWidget* widget);
 
-        QSlider* slider = new QSlider(Qt::Horizontal);
-        slider->setMinimum(min);
-        slider->setMaximum(max);
-        slider->setValue(def);
-        slider->setMinimumWidth(100);
+    static QHBoxLayout* wrap(const std::string& label, QLayout* layout);
 
-        QWrapper::QSpinBoxExt* display = new QWrapper::QSpinBoxExt;
-        display->setMinimum(min);
-        display->setMaximum(max);
-        display->setValue(def);
-
-        internal_layout->addWidget(new QLabel(name.c_str()));
-        internal_layout->addWidget(slider);
-        internal_layout->addWidget(display);
-
-        layout->addLayout(internal_layout);
-
-        QObject::connect(slider, SIGNAL(valueChanged(int)),     display, SLOT(setValue(int)));
-        QObject::connect(slider, SIGNAL(rangeChanged(int,int)), display, SLOT(setRange(int,int)));
-        QObject::connect(display, SIGNAL(valueChanged(int)), slider, SLOT(setValue(int)));
-
-
-        return slider;
-    }
-
-    static QDoubleSlider* makeDoubleSlider(QBoxLayout* layout, const std::string& name, double def, double min, double max, double step_size) {
-        QHBoxLayout* internal_layout = new QHBoxLayout;
-
-        QDoubleSlider* slider = new QDoubleSlider(Qt::Horizontal, step_size);
-        slider->setDoubleMinimum(min);
-        slider->setDoubleMaximum(max);
-        slider->setDoubleValue(def);
-        slider->setMinimumWidth(100);
-
-        QWrapper::QDoubleSpinBoxExt* display = new  QWrapper::QDoubleSpinBoxExt;
-        display->setDecimals(std::log10(1.0 / step_size));
-        display->setMinimum(min);
-        display->setMaximum(max);
-        display->setValue(def);
-
-        internal_layout->addWidget(new QLabel(name.c_str()));
-        internal_layout->addWidget(slider);
-        internal_layout->addWidget(display);
-
-        layout->addLayout(internal_layout);
-
-        QObject::connect(slider, SIGNAL(valueChanged(double)), display, SLOT(setValue(double)));
-        QObject::connect(slider, SIGNAL(rangeChanged(double,double)), display, SLOT(setRange(double, double)));
-        QObject::connect(display, SIGNAL(valueChanged(double)), slider, SLOT(setDoubleValue(double)));
-
-        return slider;
-    }
-
-    static QWidget* wrapLayout(QBoxLayout *l, QWidget *parent = 0)
-    {
-        QWidget *container = new QWidget(parent);
-        container->setLayout(l);
-        return container;
-    }
-
-    static QHBoxLayout* wrap(const std::string& label, QWidget* widget) {
-
-        QHBoxLayout* internal_layout = new QHBoxLayout;
-
-        internal_layout->addWidget(new QLabel(label.c_str()));
-        internal_layout->addWidget(widget);
-
-        return internal_layout;
-    }
-
-    static QHBoxLayout* wrap(const std::string& label, QLayout* layout) {
-
-        QHBoxLayout* internal_layout = new QHBoxLayout;
-
-        internal_layout->addWidget(new QLabel(label.c_str()));
-        internal_layout->addLayout(layout);
-
-        return internal_layout;
-    }
-
-    static void clearLayout(QLayout* layout) {
-        QLayoutItem* item;
-        while((item = layout->takeAt(0)) != NULL) {
-            if(item->layout()) {
-                clearLayout(item->layout());
-            }
-            if(item->widget()) {
-                delete item->widget();
-            }
-            delete item;
-        }
-
-    }
+    static void clearLayout(QLayout* layout);
 };
 
 #endif // QT_HELPER_HPP
