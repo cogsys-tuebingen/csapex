3f7f6299d98c7a40587c87bcad273c0a762b6f5d
buck <sebastian.buck@uni-tuebingen.de>
2013-12-25 22:08:42 +0100

    helper scripts

---
+++ plugins/check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+bld=$(tput bold)       # Bold
+blu=$(tput setaf 4)    # Blue
+rst=$(tput sgr0)       # Text reset
+
+for f in *; do
+  if [[ -d $f ]]; then
+    cd $f
+    echo
+    echo "${bld}${blu}checking $f${rst}"
+    git status
+    cd -
+  fi
+done
