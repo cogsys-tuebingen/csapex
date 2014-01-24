5753422c68f396995802e5f6551153acd260c7d0
buck <sebastian.buck@uni-tuebingen.de>
2014-01-24 15:58:35 +0100

    added pc to intensity image node

---
+++ src/csapex_point_cloud/src/pointcloud_to_intensityimage.h
@@ -0,0 +1,31 @@
+#ifndef POINTCLOUD_TO_INTENSITYIMAGE_H
+#define POINTCLOUD_TO_INTENSITYIMAGE_H
+
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex_point_cloud/point_cloud_message.h>
+
+namespace csapex
+{
+
+class PointCloudToIntensityImage : public Node
+{
+public:
+    PointCloudToIntensityImage();
+
+    virtual void setup();
+    virtual void allConnectorsArrived();
+
+    template <class PointT>
+    void inputCloud(typename pcl::PointCloud<PointT>::Ptr cloud);
+
+    void inputCloudImpl(typename pcl::PointCloud<pcl::PointXYZI>::Ptr cloud);
+
+private:
+    ConnectorIn* input_;
+    ConnectorOut* output_;
+};
+
+}
+
+#endif // POINTCLOUD_TO_INTENSITYIMAGE_H
