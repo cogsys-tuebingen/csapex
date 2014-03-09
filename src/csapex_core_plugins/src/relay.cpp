f67f984e9ec8aaee52216e07ba2d0fda419c198c
buck <sebastian.buck@uni-tuebingen.de>
2014-03-09 21:38:07 +0100

    ported relay

--- src/csapex_core_plugins/src/relay.cpp
+++ src/csapex_core_plugins/src/relay.cpp
@@ -2,7 +2,6 @@
 #include "relay.h"
 
 /// PROJECT
-
 #include <csapex/model/connector_in.h>
 #include <csapex/model/connector_out.h>
 #include <csapex/model/connection_type.h>
@@ -23,20 +22,19 @@ Relay::Relay()
     setIcon(QIcon(":/buffer.png"));
 }
 
-void Relay::fill(QBoxLayout *layout)
+void Relay::setup()
 {
-    if(input_ == NULL) {
-        input_ = addInput<connection_types::AnyMessage>("Anything");
-        output_ = addOutput<connection_types::AnyMessage>("Same as input");
-    }
+    setSynchronizedInputs(true);
+
+    input_ = addInput<connection_types::AnyMessage>("Anything");
+    output_ = addOutput<connection_types::AnyMessage>("Same as input");
 }
 
-void Relay::messageArrived(ConnectorIn *source)
+void Relay::process()
 {
-    ConnectionType::Ptr msg = source->getMessage<ConnectionType>();
+    ConnectionType::Ptr msg = input_->getMessage<ConnectionType>();
 
     output_->setType(input_->getType());
-
     output_->publish(msg);
 }
 
--- src/csapex_core_plugins/src/relay.h
+++ src/csapex_core_plugins/src/relay.h
@@ -2,7 +2,7 @@
 #define RELAY_H
 
 /// PROJECT
-#include <csapex/model/boxed_object.h>
+#include <csapex/model/node.h>
 #include <csapex/model/connection_type.h>
 
 /// SYSTEM
@@ -10,15 +10,13 @@
 
 namespace csapex {
 
-class Relay : public BoxedObject
+class Relay : public Node
 {
-    Q_OBJECT
-
 public:
     Relay();
 
-    virtual void fill(QBoxLayout* layout);
-    virtual void messageArrived(ConnectorIn* source);
+    virtual void setup();
+    virtual void process();
 
 private:
     ConnectorIn* input_;
