763679224a683ecc60e7d24a92a7c11ceb382437
rauscher <g.rauscher@uni-tuebingen.de>
2014-03-17 11:49:40 +0100

    Added methodes using PCL that finds a sphere and publishes the inliers

--- src/csapex_point_cloud/src/fit_cone.cpp
+++ src/csapex_point_cloud/src/fit_cone.cpp
@@ -13,6 +13,7 @@ CSAPEX_REGISTER_CLASS(csapex::FitCone, csapex::Node)
 
 using namespace csapex;
 using namespace csapex::connection_types;
+using namespace std;
 
 struct NamedMap {
     std::map<std::string, double> value;
@@ -22,7 +23,6 @@ FitCone::FitCone()
 {
     addTag(Tag::get("PointCloud"));
 }
-
 
 
 void FitCone::process()
@@ -39,25 +39,110 @@ void FitCone::setup()
     out_text_= addOutput<StringMessage>("String");
 
     out_params_ = addOutput<GenericMessage<NamedMap> >("Parameters");
+    out_cloud_ = addOutput<PointCloudMessage>("PointCloud");
 }
 
 
 template <class PointT>
 void FitCone::inputCloud(typename pcl::PointCloud<PointT>::Ptr cloud)
 {
-    int c = cloud->points.size();
-    //number_->display(c);
-    //output_->publish(std::string(c));
-    std::stringstream stingstream;
-    stingstream << c;
-    StringMessage::Ptr text_msg(new StringMessage);
-    text_msg->value = stingstream.str();
-    out_text_->publish(text_msg);
+    std::stringstream stringstream;
 
+    if (out_cloud_->isConnected()) {
+        //typename pcl::PointCloud<PointT>::Ptr cloud_extracted;
+        //cloud_extracted.reset(new pcl::PointCloud<PointT>);
+        typename pcl::PointCloud<PointT>::Ptr cloud_extracted(new pcl::PointCloud<PointT>);
+        pcl::ModelCoefficients::Ptr coefficients_shape (new pcl::ModelCoefficients);
+        printf("Try to find cone !!! ");
+        findCone<PointT>(cloud, cloud_extracted, coefficients_shape);
+        //cloud_extracted = cloud; // this works
+        PointCloudMessage::Ptr cloud_msg(new PointCloudMessage);
+        cloud_msg->value = cloud_extracted;
+        out_cloud_->publish(cloud_msg);
+
+        stringstream << "found [" << shape_inliers_ <<  "] inliers";
+        //stringstream << "Cone apex: "<< coefficients_shape->values[0] << ", " << coefficients_shape->values[1] << ", "<< coefficients_shape->values[2] << ", opening angle: " << coefficients_shape->values[6];
+    } else
+    {
+        stringstream << "No output cloud connected";
+    }
 
     GenericMessage<NamedMap>::Ptr param_msg(new GenericMessage<NamedMap>);
     out_params_->publish(param_msg);
 
 
+    StringMessage::Ptr text_msg(new StringMessage);
+    text_msg->value = stringstream.str();
+    out_text_->publish(text_msg);
 
-} //
+
+// Description of Cone parameters: http://docs.pointclouds.org/1.6.0/classpcl_1_1_sample_consensus_model_cone.html
+//    apex.x : the X coordinate of cone's apex
+//    apex.y : the Y coordinate of cone's apex
+//    apex.z : the Z coordinate of cone's apex
+//    axis_direction.x : the X coordinate of the cone's axis direction
+//    axis_direction.y : the Y coordinate of the cone's axis direction
+//    axis_direction.z : the Z coordinate of the cone's axis direction
+//    opening_angle : the cone's opening angle
+
+}
+
+template <class PointT>
+void FitCone::findCone(typename pcl::PointCloud<PointT>::Ptr  cloud_in, typename pcl::PointCloud<PointT>::Ptr cloud_extracted, pcl::ModelCoefficients::Ptr coefficients_shape)
+{
+    //pcl::ExtractIndices<pcl::Normal> extract_normals_;
+    pcl::ExtractIndices<PointT> extract_points;
+    pcl::SACSegmentationFromNormals<PointT, pcl::Normal> segmenter;
+    initializeSegmentater<PointT>(segmenter);
+
+    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+    estimateNormals<PointT>(cloud_in, normals);
+
+    // Segment and extract the found points
+    segmenter.setInputCloud(cloud_in);
+    segmenter.setInputNormals(normals);
+
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    segmenter.segment(*inliers, *coefficients_shape);
+    shape_inliers_ = inliers->indices.size();
+
+    extract_points.setInputCloud(cloud_in);
+    extract_points.setIndices(inliers);
+    extract_points.setNegative(false);
+    extract_points.filter(*cloud_extracted);
+}
+
+template <class PointT>
+void FitCone::estimateNormals(typename pcl::PointCloud<PointT>::Ptr cloud, pcl::PointCloud<pcl::Normal>::Ptr normals)
+{
+    typename pcl::NormalEstimation<PointT, pcl::Normal> normal_estimation;
+    typename pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
+
+    normal_estimation.setSearchMethod (tree);
+    normal_estimation.setInputCloud (cloud);
+    normal_estimation.setKSearch (50);
+    normal_estimation.compute (*normals);
+}
+
+template <class PointT>
+void FitCone::initializeSegmentater(pcl::SACSegmentationFromNormals<PointT, pcl::Normal>  &segmenter)
+{
+    int ransac = pcl::SAC_RANSAC;
+    int iterations = 7000;
+
+    double normal_distance_weight = 0.085;
+    double distance_threshold = 0.00998;
+
+    double sphere_r_min_ = 0.01;
+    double sphere_r_max_ = 0.8;
+
+    segmenter.setOptimizeCoefficients (true);
+    segmenter.setModelType (pcl::SACMODEL_SPHERE);
+    segmenter.setMethodType (ransac);
+    segmenter.setMaxIterations (iterations);
+    segmenter.setDistanceThreshold (distance_threshold);
+    segmenter.setRadiusLimits (sphere_r_min_, sphere_r_max_);
+    segmenter.setNormalDistanceWeight (normal_distance_weight);
+}
+
+//
--- src/csapex_point_cloud/src/fit_cone.h
+++ src/csapex_point_cloud/src/fit_cone.h
@@ -5,6 +5,14 @@
 #include <csapex/model/node.h>
 #include <csapex_point_cloud/point_cloud_message.h>
 #include <csapex_core_plugins/string_message.h>
+
+/// POINT CLOUD
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d.h>
+
 namespace csapex {
 class FitCone : public csapex::Node
 {
@@ -21,6 +29,16 @@ private:
     ConnectorIn* input_;
     ConnectorOut* out_text_;
     ConnectorOut* out_params_;
+    ConnectorOut* out_cloud_;
+
+    int shape_inliers_;
+
+    template <class PointT>
+    void findCone(typename pcl::PointCloud<PointT>::Ptr  cloud_in, typename pcl::PointCloud<PointT>::Ptr cloud_extracted, pcl::ModelCoefficients::Ptr coefficients_shape);
+    template <class PointT>
+    void estimateNormals(typename pcl::PointCloud<PointT>::Ptr cloud, pcl::PointCloud<pcl::Normal>::Ptr normals);
+    template <class PointT>
+    void initializeSegmentater(pcl::SACSegmentationFromNormals<PointT, pcl::Normal>  &segmenter);
 };
 
 } // namespace csapex
