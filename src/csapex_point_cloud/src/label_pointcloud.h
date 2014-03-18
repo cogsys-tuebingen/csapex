3e03835513d29c42553bbb47d9ad52dca41548db
Richard Hanten <richard.hanten@uni-tuebingen.de>
2014-03-18 22:58:42 +0100

    label and color pointclouds

--- src/csapex_point_cloud/src/label_pointcloud.h
+++ src/csapex_point_cloud/src/label_pointcloud.h
@@ -1,10 +1,28 @@
 #ifndef LABEL_POINTCLOUD_H
 #define LABEL_POINTCLOUD_H
 
-class LabelPointCloud
+/// PROJECT
+#include <csapex/model/node.h>
+#include <csapex_point_cloud/point_cloud_message.h>
+#include <opencv2/core/core.hpp>
+
+namespace csapex {
+class LabelPointCloud : public csapex::Node
 {
 public:
     LabelPointCloud();
-};
 
+    virtual void process();
+    virtual void setup();
+
+    template <class PointT>
+    void inputCloud(typename pcl::PointCloud<PointT>::Ptr cloud);
+
+protected:
+    ConnectorIn*  input_;
+    ConnectorIn*  labels_;
+    ConnectorOut* output_;
+    cv::Mat       label_mat_;
+};
+}
 #endif // LABEL_POINTCLOUD_H
