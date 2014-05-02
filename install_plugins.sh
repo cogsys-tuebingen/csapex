572a50b2ba7aecdc5f79a9299572a464ae1d4178
Robert Pech <robert.pech@uni-tuebingen.de>
2014-05-02 10:52:14 +0200

    paths have changed

--- install_plugins.sh
+++ install_plugins.sh
@@ -3,5 +3,5 @@
 git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/core-plugins.git plugins/core_plugins
 git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/vision-plugins.git plugins/vision_plugins
 git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/backgroundsubtraction.git plugins/background_subtraction
-git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/cip104.git plugins/cip104
-git clone gitlab@gitlab.cs.uni-tuebingen.de:csapex/patsy.git plugins/patsy
+git clone gitlab@gitlab.cs.uni-tuebingen.de:ioc/cip104.git plugins/cip104
+git clone gitlab@gitlab.cs.uni-tuebingen.de:patsy/patsy-apex.git plugins/patsy
