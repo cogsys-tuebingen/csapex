0ace600a5ea7858eda84bbba86e5d322ca4664fd
buck <sebastian.buck@uni-tuebingen.de>
2016-08-10 00:13:16 +0200

    updated script collection

--- plugins/deletebranch.sh
+++ plugins/deletebranch.sh
@@ -3,15 +3,21 @@ bld=$(tput bold)       # Bold
 blu=$(tput setaf 4)    # Blue
 rst=$(tput sgr0)       # Text reset
 
+branch=$1
+
+if [[ ! $branch ]]; then
+	echo "no branch specified"
+	exit
+fi
+
 for f in *; do
   if [[ -d $f ]]; then
     cd $f
     if [[ -d .git ]]; then
-      echo "${bld}${blu}checking $f${rst}"
-      if [[ $1 ]] ; then
-        git push $@
-      else
-        git push origin master
+      test=$(git branch | grep $branch)
+      if [[ $test ]]; then
+        echo "${bld}${blu}delete $branch in $f${rst}"
+        git branch -d "$branch"
       fi
     fi
     cd - 1> /dev/null
