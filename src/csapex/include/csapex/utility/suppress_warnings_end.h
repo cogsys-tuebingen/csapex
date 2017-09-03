2a7c032f22260504d43138d79bb28846e80fae02
buck <sebastian.buck@uni-tuebingen.de>
2017-09-03 13:20:52 +0200

    Added headers to selectively suppress warnings from included files

---
+++ src/csapex/include/csapex/utility/suppress_warnings_end.h
@@ -0,0 +1,11 @@
+#undef BOOST_SIGNALS_NO_DEPRECATION_WARNING
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+
+#elif defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC diagnostic pop
+
+#elif defined(_MSC_VER)
+
+#endif
---
+++ src/csapex/include/csapex/utility/suppress_warnings_start.h
@@ -0,0 +1,18 @@
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wall"
+#pragma clang diagnostic ignored "-Wpedantic"
+
+#elif defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
+#pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wformat"
+
+#elif defined(_MSC_VER)
+
+#endif
+
+#define BOOST_SIGNALS_NO_DEPRECATION_WARNING
