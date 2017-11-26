8723234fdf5d3bb33b3680e8e9488ba67ad145e4
buck <sebastian.buck@uni-tuebingen.de>
2017-11-26 22:57:46 +0100

    Implemented a workaround for GCC bug 41933 (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=41933)

--- src/csapex/include/csapex/view/utility/qobserver.h
+++ src/csapex/include/csapex/view/utility/qobserver.h
@@ -7,10 +7,51 @@
 
 /// SYSTEM
 #include <mutex>
+#include <functional>
 #include <QObject>
 
 namespace csapex
 {
+
+#ifdef __GNUC__
+#  include <features.h>
+#  if __GNUC_PREREQ(4,9)
+#define USE_GCC47_WORKAROUND 0
+#  else
+#define USE_GCC47_WORKAROUND 1
+#  endif
+#else
+//    If not gcc
+#endif
+
+#if USE_GCC47_WORKAROUND
+namespace gcc47
+{
+
+template<int ...>
+struct seq { };
+
+template<int N, int ...S>
+struct generateSequence : generateSequence<N-1, N-1, S...> { };
+
+template<int ...S>
+struct generateSequence<0, S...> {
+    typedef seq<S...> type;
+};
+
+template<typename Function, typename Tuple, int ...S>
+void call(seq<S...>, Function F, Tuple tuple) {
+    (void) tuple;
+    F(std::get<S>(tuple) ...);
+}
+
+template<typename Instance, typename Function, typename Tuple, int ...S>
+void call(seq<S...>, Instance* i, Function F, Tuple tuple) {
+    (void) tuple;
+    (i->*F)(std::get<S>(tuple) ...);
+}
+}
+#endif //USE_GCC47_WORKAROUND
 
 class CSAPEX_QT_EXPORT QObserver : public QObject, public Observer
 {
@@ -24,30 +65,52 @@ public:
     void observeQueued(slim_signal::Signal<Result(Args...)>& signal,
                        Lambda callback)
     {
-        manageConnection(signal.connect([=](Args... args) {
+        auto lambda = [=](Args... args) {
             {
                 std::unique_lock<std::recursive_mutex> lock(observation_queue_mutex_);
+
+#if USE_GCC47_WORKAROUND
+                std::tuple<Args...> args_tuple(args...);
+#endif //USE_GCC47_WORKAROUND
+
                 observation_queue_.emplace_back([=](){
+#if USE_GCC47_WORKAROUND
+                    gcc47::call(typename gcc47::generateSequence<sizeof...(Args)>::type(), callback, args_tuple);
+#else  //USE_GCC47_WORKAROUND
                     callback(args...);
+#endif //USE_GCC47_WORKAROUND
                 });
             }
             Q_EMIT handleObservationsRequest();
-        }));
+        };
+
+        manageConnection(signal.connect(lambda));
     }
 
     template <typename Lambda, typename Result, typename... Args>
     void observeQueued(std::shared_ptr<slim_signal::Signal<Result(Args...)>>& signal,
                        Lambda callback)
     {
-        manageConnection(signal->connect([=](Args... args) {
+        auto fn = [=](Args... args) {
             {
                 std::unique_lock<std::recursive_mutex> lock(observation_queue_mutex_);
+
+#if USE_GCC47_WORKAROUND
+                std::tuple<Args...> args_tuple(args...);
+#endif //USE_GCC47_WORKAROUND
+
                 observation_queue_.emplace_back([=](){
+#if USE_GCC47_WORKAROUND
+                    gcc47::call(typename gcc47::generateSequence<sizeof...(Args)>::type(), callback, args_tuple);
+#else  //USE_GCC47_WORKAROUND
                     callback(args...);
+#endif //USE_GCC47_WORKAROUND
                 });
             }
             Q_EMIT handleObservationsRequest();
-        }));
+        };
+
+        manageConnection(signal->connect(fn));
     }
 
 
@@ -58,15 +121,26 @@ public:
                        Receiver* instance,
                        Result (Receiver::*function)(Args...))
     {
-        manageConnection(signal.connect([=](Args... args) {
+        auto fn = [=](Args... args) {
             {
                 std::unique_lock<std::recursive_mutex> lock(observation_queue_mutex_);
+
+#if USE_GCC47_WORKAROUND
+                std::tuple<Args...> args_tuple(args...);
+#endif //USE_GCC47_WORKAROUND
+
                 observation_queue_.emplace_back([=](){
+#if USE_GCC47_WORKAROUND
+                    gcc47::call(typename gcc47::generateSequence<sizeof...(Args)+1>::type(), instance, function, args_tuple);
+#else  //USE_GCC47_WORKAROUND
                     (instance->*function)(args...);
+#endif //USE_GCC47_WORKAROUND
                 });
             }
             Q_EMIT handleObservationsRequest();
-        }));
+        };
+
+        manageConnection(signal.connect(fn));
     }
 
     template <typename Receiver, typename Result, typename... Args>
@@ -74,15 +148,26 @@ public:
                        Receiver* instance,
                        Result (Receiver::*function)(Args...))
     {
-        manageConnection(signal->connect([=](Args... args) {
+        auto fn = [=](Args... args) {
             {
                 std::unique_lock<std::recursive_mutex> lock(observation_queue_mutex_);
+
+#if USE_GCC47_WORKAROUND
+                std::tuple<Args...> args_tuple(args...);
+#endif //USE_GCC47_WORKAROUND
+
                 observation_queue_.emplace_back([=](){
+#if USE_GCC47_WORKAROUND
+                    gcc47::call(typename gcc47::generateSequence<sizeof...(Args)>::type(), instance, function, args_tuple);
+#else  //USE_GCC47_WORKAROUND
                     (instance->*function)(args...);
+#endif //USE_GCC47_WORKAROUND
                 });
             }
             Q_EMIT handleObservationsRequest();
-        }));
+        };
+
+        manageConnection(signal->connect(fn));
     }
 
 
