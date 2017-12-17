50329f1bbd8fc71ed9307a458df2c818839ac395
buck <sebastian.buck@uni-tuebingen.de>
2017-12-18 00:40:03 +0100

    Refactored MessageTemplate copy ctors and assignment operators

--- src/csapex/include/csapex/msg/message_template.hpp
+++ src/csapex/include/csapex/msg/message_template.hpp
@@ -14,12 +14,57 @@ namespace csapex
 namespace connection_types
 {
 
+
+/**
+ * @brief The MessageTemplateContainer struct wraps non integral types.
+ *        The class derives from the data type so that it can be dynamically casted to and from it.
+ *        A reference 'value' is kept up to date, which will always refer to the value that is derived from.
+ * @tparam Type the type to wrap
+ * @tparam integral true, iff type is integral
+ */
 template <typename Type, bool integral>
 struct MessageTemplateContainer : public Type
 {
     using value_type = Type;
 
-    Type& getValue() {
+    MessageTemplateContainer()
+        : value(*this)
+    {}
+    MessageTemplateContainer(const MessageTemplateContainer& other)
+        : Type(other), value(*this)
+    {
+        *this = other.value;
+    }
+    MessageTemplateContainer(const Type& v)
+        : value(*this)
+    {
+        value = v;
+    }
+    MessageTemplateContainer(Type&& v)
+        : value(*this)
+    {
+        value = std::move(v);
+    }
+
+    MessageTemplateContainer& operator = (const MessageTemplateContainer & copy)
+    {
+        value = copy.value;
+        return *this;
+    }
+    MessageTemplateContainer& operator = (MessageTemplateContainer&& copy)
+    {
+        value = std::move(copy.value);
+        return *this;
+    }
+
+    MessageTemplateContainer& operator = (const Type & other_value)
+    {
+        value = other_value;
+        return *this;
+    }
+    MessageTemplateContainer& operator = (Type && other_value)
+    {
+        value = std::move(other_value);
         return *this;
     }
 
@@ -27,29 +72,44 @@ struct MessageTemplateContainer : public Type
     {
         return dynamic_cast<const Type*>(other_side);
     }
+
+public:
+    Type& value;
 };
 
+/**
+ * @brief The MessageTemplateContainer struct wraps non integral types.
+ *        This is a specialization for integral types, which we cannot derive from.
+ * @tparam Type the type to wrap
+ * @tparam integral true, iff type is integral
+ */
 template <typename Type>
 struct MessageTemplateContainer<Type, true>
 {
     using value_type = Type;
-
-    Type& getValue() {
-        return value_impl;
-    }
 
     static bool acceptsConnectionFrom(const TokenData* other_side)
     {
         return dynamic_cast<const MessageTemplateContainer<Type, true>*>(other_side);
     }
 
-private:
-    Type value_impl;
+public:
+    Type value;
 };
 
+/**
+ * @brief The MessageTemplateBase struct is a marker for RTTI
+ */
 struct MessageTemplateBase
 {};
 
+/**
+ * @brief The MessageTemplate class generates a message from any datatype
+ * @tparam Type The type to wrap in this message
+ * @tparam Instance The instance that derives from this class
+ *         This uses the "curiously recurring template pattern", so child classes must be of the form
+ *         class Instance : public MessagesTemplate<Type, Instance>
+ */
 template <typename Type, class Instance>
 class MessageTemplate : public Message, public MessageTemplateContainer<Type, std::is_integral<Type>::value>, public MessageTemplateBase
 {
@@ -57,19 +117,32 @@ public:
     typedef std::shared_ptr<Instance> Ptr;
     typedef std::shared_ptr<const Instance> ConstPtr;
 
-    typedef MessageTemplateContainer<Type, std::is_integral<Type>::value> Parent;
+    typedef MessageTemplateContainer<Type, std::is_integral<Type>::value> ValueContainer;
     typedef MessageTemplate<Type,Instance> Self;
 
     MessageTemplate( const std::string& frame_id = "/", Message::Stamp stamp = 0)
-        : Message(type<Instance>::name(), frame_id, stamp),
-          value(Parent::getValue())
+        : Message(type<Instance>::name(), frame_id, stamp)
+    {}
+
+    MessageTemplate( const Self& copy )
+        : Message(type<Instance>::name(), copy.frame_id, copy.stamp_micro_seconds),
+          ValueContainer(static_cast<const ValueContainer&>(copy))
+    {}
+
+    MessageTemplate( Self&& moved )
+        : Message(type<Instance>::name(), moved.frame_id, moved.stamp_micro_seconds),
+          ValueContainer(static_cast<ValueContainer&&>(moved))
     {}
 
 
     Self& operator = (const Self& other)
     {
-        Parent::operator=(other);
-        value = Parent::getValue();
+        ValueContainer::operator=(other);
+        return *this;
+    }
+    Self& operator = (Self&& other)
+    {
+        ValueContainer::operator=(std::move(other));
         return *this;
     }
 
@@ -87,10 +160,8 @@ public:
 
     bool acceptsConnectionFrom(const TokenData* other_side) const override
     {
-        return Parent::acceptsConnectionFrom(other_side);
+        return ValueContainer::acceptsConnectionFrom(other_side);
     }
-
-    Type& value;
 
 protected:
     Ptr cloneInstance() const
@@ -98,7 +169,7 @@ protected:
         Ptr new_msg(new Instance);
         new_msg->frame_id = frame_id;
         new_msg->stamp_micro_seconds = stamp_micro_seconds;
-        new_msg->value = value;
+        new_msg->value = ValueContainer::value;
         return new_msg;
     }
 
