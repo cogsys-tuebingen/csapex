5b330bbeeba0d7a89253749d6c51383ba8158e69
buck <sebastian.buck@uni-tuebingen.de>
2018-02-18 03:55:58 +0100

    Add regression test node
    csapex_regression_test goes through all plugin packages and executes available regression tests.
    These are specified in the package.xml file.
    
    Example:
      <export>
        <csapex plugin="${prefix}/plugins.xml"
                snippets="${prefix}/snippets"
                regression_tests="${prefix}/tests/regression" />
      </export>
    
    All files in ${prefix}/tests/regression will be executed. The file names should follow the convention
      <return code>_<file name>.apex
    where the expected return code is followed by a descriptive name.
    
    Regression configs have to either send a message to the global 'exit' slot if everything went okay,
    or to the 'abort' slot, if an assertion was violated.

---
+++ src/csapex_testing/src/regression_tester.cpp
@@ -0,0 +1,118 @@
+#include <csapex/plugin/plugin_locator.h>
+#include <csapex/core/settings/settings_impl.h>
+#include <csapex/core/csapex_core.h>
+#include <csapex/core/exception_handler.h>
+#include <csapex/utility/subprocess.h>
+
+#include <iostream>
+
+#include <boost/filesystem.hpp>
+#include <boost/version.hpp>
+
+#if (BOOST_VERSION / 100000) >= 1 && (BOOST_VERSION / 100 % 1000) >= 54
+namespace bf3 = boost::filesystem;
+#else
+namespace bf3 = boost::filesystem3;
+#endif
+
+using namespace csapex;
+
+int main(int argc, char *argv[])
+{
+    ExceptionHandler eh(false);
+    SettingsImplementation settings;
+
+    std::string path_to_bin(argv[0]);
+    settings.set("path_to_bin", path_to_bin);
+
+    CsApexCore core(settings, eh);
+    PluginLocatorPtr locator = core.getPluginLocator();
+
+    std::vector<bf3::path> reg_test_files;
+    for(const std::string& dir_string : locator->getPluginPaths("regression_tests")) {
+
+        boost::filesystem::path directory(dir_string);
+
+        boost::filesystem::directory_iterator dir(directory);
+        boost::filesystem::directory_iterator end;
+
+        for(; dir != end; ++dir) {
+            boost::filesystem::path path = dir->path();
+            reg_test_files.push_back(path);
+        }
+    }
+
+    bool error_happened = false;
+
+    for(const bf3::path& reg_test : reg_test_files) {
+
+        std::string filename = reg_test.filename().string();;
+        int expected_return_code = 0;
+
+        // The files must be named <return code>_<file name>.apex
+        std::size_t split = filename.find_first_of("_");
+        if(split == std::string::npos) {
+            std::cout << "Warning: Do not know the expected return code of test file '" << filename << "'\n";
+            std::cout << "         The file does not follow the convention <return code>_<file name>.apex" << '\n';
+            std::cout << "         Assuming return code 0" << std::endl;
+        } else {
+            std::stringstream conv(filename.substr(0, split));
+            conv >> expected_return_code;
+            filename = filename.substr(split + 1);
+        }
+
+        std::cout << "[ RUN APEX REGRESSION TEST ] " << filename << std::endl;
+
+        {
+            Subprocess sp(reg_test.filename().string());
+            sp.fork([&]() {
+                chdir(reg_test.parent_path().string().c_str());
+
+                core.load(reg_test.string());
+
+                std::mutex running_mutex;
+                std::condition_variable shutdown;
+                auto connection = core.shutdown_requested.connect([&](){
+                    shutdown.notify_all();
+                });
+
+                core.startMainLoop();
+                auto lock = std::unique_lock<std::mutex>(running_mutex);
+                shutdown.wait_for(lock, std::chrono::seconds(5));
+
+                if(core.isMainLoopRunning()) {
+                    std::cerr << "Test timed out!" << std::endl;
+                    core.abort();
+                }
+
+                core.joinMainLoop();
+
+                return core.getReturnCode();
+            });
+
+            int return_code = sp.join();
+            if(return_code == expected_return_code) {
+                std::cout << "[                       OK ] " << std::endl;
+
+            } else {
+                error_happened = true;
+
+                std::cout << "[                   FAILED ] Error code: " << return_code << ", exepected: " << expected_return_code << std::endl;
+
+                std::string out = sp.getChildStdOut();
+                if(!out.empty()) {
+                    std::cout << "Ouput was: \n";
+                    std::cout << out << std::endl;
+                }
+
+                std::string err = sp.getChildStdErr();
+                if(!err.empty()) {
+                    std::cout << "Error Ouput was: \n";
+                    std::cout << err << std::endl;
+                }
+            }
+        }
+    }
+
+    return error_happened ? 1 : 0;
+}
