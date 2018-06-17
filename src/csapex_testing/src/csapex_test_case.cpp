2d88eec0d2d50ccf48ee81cc2d2b5ad7e9f2c7d0
buck <sebastian.buck@uni-tuebingen.de>
2018-06-17 23:16:02 +0200

    Add new testing base class that does error handling

---
+++ src/csapex_testing/src/csapex_test_case.cpp
@@ -0,0 +1,17 @@
+/// HEADER
+#include <csapex_testing/csapex_test_case.h>
+
+/// PROJECT
+#include <csapex/utility/error_handling.h>
+
+using namespace csapex;
+
+CsApexTestCase::CsApexTestCase()
+{
+    csapex::error_handling::init();
+}
+
+CsApexTestCase::~CsApexTestCase()
+{
+
+}
